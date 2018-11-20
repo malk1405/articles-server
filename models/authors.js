c7738fedf16bd6b136daa807878f60709ce05ba1
malk1405 <a.a.malkin@mai.ru>
2018-11-20 11:22:49 +0300

    Добавил модель авторов

---
+++ models/authors.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+require("mongoose-type-email");
+
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  birthDate: {
+    type: Date
+  },
+
+  email: {
+    type: mongoose.SchemaTypes.Email
+  }
+});
+
+const Author = mongoose.model("Author", authorSchema);
+
+module.exports = Author;
