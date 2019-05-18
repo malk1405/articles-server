a4c80a7148cdb3a9ff80e7c04f0553387c69683e
malk1405 <a.a.malkin@mai.ru>
2019-05-18 11:21:17 +0300

    Сделали чтобы вывод зависел от страницы, которой пришел запрос

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,23 +3,29 @@ const { Author } = require("../models/authors");
 const { catchErr } = require("../utils/error");
 
 module.exports = {
-  search: async function({ query: { value, type } }, res, next) {
+  search: async function({ query: { value, page } }, res, next) {
     try {
-      const articlesNumber = await Article.find({
+      const articles = await Article.find({
         $or: [
           { title: new RegExp(value, "i") },
           { "authors.name": new RegExp(value, "i") },
           { "authors.lastname": new RegExp(value, "i") }
         ]
-      }).countDocuments();
-      const authorsNumber = await Author.find({
+      });
+      const articlesNumber = articles.length;
+
+      const authors = await Author.find({
         $or: [
           { lastname: new RegExp(value, "i") },
           { name: new RegExp(value, "i") }
         ]
-      }).countDocuments();
+      });
 
-      res.json({ articlesNumber, authorsNumber });
+      const authorsNumber = authors.length;
+      if (page === "authors") {
+        return res.json({ articlesNumber, authorsNumber, authors });
+      }
+      res.json({ articlesNumber, authorsNumber, articles });
     } catch (error) {
       catchErr(error, next);
     }
